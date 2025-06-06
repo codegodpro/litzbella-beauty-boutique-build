dfb3c914c0f06d2e5d8a5e0b683de27c0d4d6106
Agada Kingsley <122119198+codegodpro@users.noreply.github.com>
2025-06-05 22:12:41 -0700

    Update ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -48,8 +48,8 @@ const ProductDetail = () => {
     // Share logic here
   };
 
-  return (
-    <div className="min-h-screen bg-gray-50">
+ return (
+    <div className="min-h-screen bg-white">
       <Header />
       
       <main className="container mx-auto px-4 py-8">
